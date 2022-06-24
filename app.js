1ed3d6ed634b53bb5363481478658e258e1b675b
nancycrojas <nanclarisaok@gmail.com>
2022-06-24 16:17:27 -0300

    Variables, datos y operadores - ejercicio 19

---
+++ app.js
@@ -0,0 +1,7 @@
+//Cantidad de caracteres
+
+//Crear un programa que pida al usuario ingresar un texto y muestre un mensaje con la cantidad de caracteres que tiene ese texto. INVESTIGAR CÓMO HACERLO
+
+const caracteres = prompt("Por favor ingresa un texto");
+alert("El texto tiene " + caracteres.length + " caracteres.");
+
